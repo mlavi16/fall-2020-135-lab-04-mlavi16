d2c173b14e615ae6ff261fb50602bea71356744d
mlavi16 <Maya.lavi11@gmail.com>
2020-09-21 12:57:14 -0400

    adding box() tests

--- main.cpp
+++ main.cpp
@@ -10,5 +10,18 @@ Assignment: Lab 4
 
 int main()
 {
-
+    // box.cpp tests
+    std::string result;
+    result = box(3,5);
+    std::cout << "box(3,5):\n";
+    std::cout << result;
+    std::cout << "\n------------------\n"; // seperator 
+    result = box(0,0);
+    std::cout << "box(0,0):\n";
+    std::cout << result;
+    std::cout << "\n------------------\n"; // seperator 
+    result = box(5, 1);
+    std::cout << "box(5,1):\n";
+    std::cout << result;
+    std::cout << "\n------------------\n"; // seperator 
 }
